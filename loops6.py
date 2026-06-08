3c59493059c6bfbd9e0dad273013792da2f87c15
Niraj2005kumar <nirajverma075@gmail.com>
2026-06-09 00:41:06 +0530

    break continue and else

--- loops6.py
+++ loops6.py
@@ -1,69 +1,86 @@
 
 
-# # # #  ////////////////////   FOR LOOP   ////////////////////////
+# # # # #  ////////////////////   FOR LOOP   ////////////////////////
 
-# # # # the range() function 
+# # # # # the range() function 
 
-# # # # range(stop)  # generates numbers from 0 to stop-1
-# # # # range(start, stop)  # generates numbers from start to stop-1
-# # # # range(start, stop, step)  # generates numbers from start to stop-1 with a step of step
-
-
-# # # list(range(5))   #(0,1,2,3,4)
-# # # list(range(1, 6))   #(1,2,3,4,5)
-# # # list(range(1, 10, 2))   #(1,3,5,7,9)
+# # # # # range(stop)  # generates numbers from 0 to stop-1
+# # # # # range(start, stop)  # generates numbers from start to stop-1
+# # # # # range(start, stop, step)  # generates numbers from start to stop-1 with a step of step
 
 
-
-# # # #FOR LOOP WITH NUMBER 
-
-# # # range(10,20,1)
-# # # range(10,90,1)
-# # # range(46)
-
-# # # for i in range(1, 21, 1):
-# # #     print(i)
+# # # # list(range(5))   #(0,1,2,3,4)
+# # # # list(range(1, 6))   #(1,2,3,4,5)
+# # # # list(range(1, 10, 2))   #(1,3,5,7,9)
 
 
-# # # #Question:- print a table of 5 using for loop and range function
-# # # range(5, 50, 5)
-# # # for i in range(5, 55, 5):
-# # #     print(i)
+
+# # # # #FOR LOOP WITH NUMBER 
+
+# # # # range(10,20,1)
+# # # # range(10,90,1)
+# # # # range(46)
+
+# # # # for i in range(1, 21, 1):
+# # # #     print(i)
+
+
+# # # # #Question:- print a table of 5 using for loop and range function
+# # # # range(5, 50, 5)
+# # # # for i in range(5, 55, 5):
+# # # #     print(i)
     
 
 
 
-# # # question
+# # # # question
 
-# # n = int(input("enter your number and print its table:-"))
+# # # n = int(input("enter your number and print its table:-"))
 
-# # for i in range(n, (n * 10) + 1, n):
+# # # for i in range(n, (n * 10) + 1, n):
+# # #     print(i)
+
+
+
+# # #//////////////////// for loop with string///////////////
+
+
+
+# # # step 1:- user se ek number input karvaye
+
+# # a = "student"
+
+# # # # for i in a :
+# # # #     print((name)i)
+
+
+# # # #step 2
+# # # for char in a :
+# # #     print(char)
+
+
+# # #step 3 
+
+# # for i in range (len(a)) :
+# #     print(f"{a}:{a[i]}")
+
+
+
+
+# #break condition and else 
+
+# #break condition
+
+
+# # for i in range(1, 11):
+# #     if i == 4:
+# #         break
 # #     print(i)
 
 
-
-# #//////////////////// for loop with string///////////////
-
-
-
-# # step 1:- user se ek number input karvaye
-
-# a = "student"
-
-# # # for i in a :
-# # #     print((name)i)
-
-
-# # #step 2
-# # for char in a :
-# #     print(char)
-
-
-# #step 3 
-
-# for i in range (len(a)) :
-#     print(f"{a}:{a[i]}")
-
-
-sjdkaskdoq
+# #continue condition
+# for i in range (1 , 11 ):
+#     if i == 4 or i == 7 or i == 9:
+#         continue 
+#     print(i)
 
