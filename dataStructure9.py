18d4210d2f419841e1b56d4dc4a12c3ada5cec93
Niraj2005kumar <nirajverma075@gmail.com>
2026-06-14 23:13:52 +0530

    list question

--- dataStructure9.py
+++ dataStructure9.py
@@ -21,19 +21,19 @@
 
 # list //////////////////////////
 
-a = [12,32,665,78,98,78]
-print(type(a))
+# a = [12,32,665,78,98,78]
+# print(type(a))
 
 # ordered nature you can access any element 
-a = [12,32,665,78,98,78]
-print(a[1])
+# a = [12,32,665,78,98,78]
+# print(a[1])
 
 
 # mutable nature :- you can change any value on your list 
 
-l = [10,20,30,40]
-l[1] = 20
-print(l)
+# l = [10,20,30,40]
+# l[1] = 20
+# print(l)
 
 
 # Duplicates :- you can have duplicates value 
@@ -43,19 +43,47 @@ print(l)
 
 # traversing on list 
 
-a = [10,20,30,40 , 20]
+# a = [10,20,30,40 , 20]
 
 # traversing on values 
 
-for i in a:
-    print(i)
+# for i in a:
+#     print(i)
 
 
 # traversing on index :- esme hum directily hum value ko acces kr sakte hai 
 
-for i in range(0,len(a)):
-    print(a[i])
+# for i in range(0,len(a)):
+#     print(a[i])
 
 
+a = [10,20,30,40]
+
+# a.append("60") # adds a value to the last sport
+# a.insert(2,30) # add value in the middle 
+# a.pop()
+# a.remove(30) #directly ek value ko provide kr sakte hai means I want remove the 30 then output is [10,20,40]
+# a.clear() #all elements are remove in the index 
+# a.sort()#sort the list in ascending order and return None matlab esme element ko decrease se increase krta hai 
+# a.sort(reverse=True)# esme increase se decrease ho jata hai 
+
+
+
+print(a)
+
+# Homogeneous Data Type ka matlab hota hai ki kisi data structure ke andar saare elements ek hi type ke ho.
+# numbers = [10, 20, 30, 40]
+
+# Heterogeneous Data Type:
+# Agar alag-alag type ke elements ho to use heterogeneous data type kehte hain.
+# data = [10, "Niraj", 3.14, True]
+
+
+# LIST QUESTION/////////////////////////////
+
+
+# Question :- print all position and neagative element separatly.
+
+num
 
 
