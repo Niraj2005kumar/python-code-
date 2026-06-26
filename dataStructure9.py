76752199d57409f87d7a826cd9ea51b6f096d296
Niraj2005kumar <nirajverma075@gmail.com>
2026-06-26 23:53:49 +0530

    Management system

--- dataStructure9.py
+++ dataStructure9.py
@@ -31,41 +31,41 @@
 
 # mutable nature :- you can change any value on your list 
 
-# l = [10,20,30,40]
-# l[1] = 20
-# print(l)
+l = [10,20,30,40]
+l[1] = 20
+print(l)
 
 
 # Duplicates :- you can have duplicates value 
 
-#t = [1,1,1,2,2,2,3,3,3,3]
+t = [1,1,1,2,2,2,3,3,3,3]
 
 
 # traversing on list 
 
-# a = [10,20,30,40 , 20]
+a = [10,20,30,40 , 20]
 
 # traversing on values 
 
-# for i in a:
-#     print(i)
+for i in a:
+    print(i)
 
 
 # traversing on index :- esme hum directily hum value ko acces kr sakte hai 
 
-# for i in range(0,len(a)):
-#     print(a[i])
+for i in range(0,len(a)):
+    print(a[i])
 
 
 a = [10,20,30,40]
 
-# a.append("60") # adds a value to the last sport
-# a.insert(2,30) # add value in the middle 
-# a.pop()
-# a.remove(30) #directly ek value ko provide kr sakte hai means I want remove the 30 then output is [10,20,40]
-# a.clear() #all elements are remove in the index 
-# a.sort()#sort the list in ascending order and return None matlab esme element ko decrease se increase krta hai 
-# a.sort(reverse=True)# esme increase se decrease ho jata hai 
+a.append("60") # adds a value to the last sport
+a.insert(2,30) # add value in the middle 
+a.pop()
+a.remove(30) #directly ek value ko provide kr sakte hai means I want remove the 30 then output is [10,20,40]
+a.clear() #all elements are remove in the index 
+a.sort()#sort the list in ascending order and return None matlab esme element ko decrease se increase krta hai 
+a.sort(reverse=True)# esme increase se decrease ho jata hai 
 
 
 
@@ -84,30 +84,30 @@ print(a)
 
 # Question :- print all position and neagative element separatly.
 
-# l = [3,5, -45,]
+l = [3,5, -45,]
 
-# pos = []
-# neg = []
+pos = []
+neg = []
 
-# for i in l:
-#     if i >= 0:
-#         pos.append(i)
-#     else:
-#         neg.append(i)
+for i in l:
+    if i >= 0:
+        pos.append(i)
+    else:
+        neg.append(i)
 
-# print(f"yore positive element is :-{pos}")
-# print(f"negative elements is :- {neg}")
+print(f"yore positive element is :-{pos}")
+print(f"negative elements is :- {neg}")
 
 
 # Question :- find the mean {average } of all element . 
 
-# l = [10,20,30,40]
+l = [10,20,30,40]
 
-# sum = 0
-# for i in l:
-#     sum = sum +i
+sum = 0
+for i in l:
+    sum = sum +i
 
-# print(f"your average is {sum/ len(l)}")
+print(f"your average is {sum/ len(l)}")
 
 
 
@@ -115,37 +115,37 @@ print(a)
 
 # Question :- find the greatest element and print its index 
 
-# a = [ 20,40,82,40,56,50,7,45,99]
+a = [ 20,40,82,40,56,50,7,45,99]
 
-# largest = a[0]
-# lowest = a[0]
+largest = a[0]
+lowest = a[0]
 
 
-# for i in a:
-#     if i > largest:
-#         largest = i
+for i in a:
+    if i > largest:
+        largest = i
 
-#     else:
-#         if i< lowest :
-#             lowest = i
+    else:
+        if i< lowest :
+            lowest = i
 
-# print(f"your lowest value is {lowest}")
-# print(f"your largest value is {largest}")
+print(f"your lowest value is {lowest}")
+print(f"your largest value is {largest}")
 
 
 # ye toh ese hi bana deiye the or humlogo ko index prnt krne ko bola gaya hai to ese liye niche wala code sahi hia index ko print krne ke liye 
 # or uper wala code hai bas lowest or largest ko print krne ke liye diya gaya hai 
 
 
-# largest = a[0]
-# index = 0
+largest = a[0]
+index = 0
 
-# for i in range(len(a)):
-#     if a[i] > largest :
-#         largest = a[0]
-#         index = i 
+for i in range(len(a)):
+    if a[i] > largest :
+        largest = a[0]
+        index = i 
 
-# print(f"your largest value is {largest} at index is {index}")
+print(f"your largest value is {largest} at index is {index}")
 
 
 
